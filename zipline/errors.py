e68c62cc287055be0ebef8c2b7b116df8de41859
vikram-narayan <vnarayan@quantopian.com>
2018-06-06 17:22:24 -0400

    MAINT: move lazyval to memoize

--- zipline/errors.py
+++ zipline/errors.py
@@ -17,56 +17,6 @@ from textwrap import dedent
 from zipline.utils.memoize import lazyval
 
 
-class lazyval(object):
-    """Decorator that marks that an attribute of an instance should not be
-    computed until needed, and that the value should be memoized.
-
-    Example
-    -------
-
-    >>> from zipline.utils.memoize import lazyval
-    >>> class C(object):
-    ...     def __init__(self):
-    ...         self.count = 0
-    ...     @lazyval
-    ...     def val(self):
-    ...         self.count += 1
-    ...         return "val"
-    ...
-    >>> c = C()
-    >>> c.count
-    0
-    >>> c.val, c.count
-    ('val', 1)
-    >>> c.val, c.count
-    ('val', 1)
-    >>> c.val = 'not_val'
-    Traceback (most recent call last):
-    ...
-    AttributeError: Can't set read-only attribute.
-    >>> c.val
-    'val'
-    """
-    def __init__(self, get):
-        self._get = get
-        self._cache = WeakKeyDictionary()
-
-    def __get__(self, instance, owner):
-        if instance is None:
-            return self
-        try:
-            return self._cache[instance]
-        except KeyError:
-            self._cache[instance] = val = self._get(instance)
-            return val
-
-    def __set__(self, instance, value):
-        raise AttributeError("Can't set read-only attribute.")
-
-    def __delitem__(self, instance):
-        del self._cache[instance]
-
-
 class ZiplineCalendarError(Exception):
     msg = None
 
