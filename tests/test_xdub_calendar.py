eb1f3cdd7ba87601da39a8bd3576f519a6f4f435
David Michalowicz <dmichalowicz@quantopian.com>
2018-10-19 16:53:31 -0400

    ENH: Add Ireland calendar

---
+++ tests/test_xdub_calendar.py
@@ -0,0 +1,155 @@
+from unittest import TestCase
+import pandas as pd
+
+from .test_trading_calendar import ExchangeCalendarTestBase
+from trading_calendars.exchange_calendar_xdub import XDUBExchangeCalendar
+
+
+class XDUBCalendarTestCase(ExchangeCalendarTestBase, TestCase):
+
+    answer_key_filename = 'xdub'
+    calendar_class = XDUBExchangeCalendar
+
+    # The XDUB is open from 8:00 am to 4:28 pm.
+    MAX_SESSION_HOURS = 8.467
+
+    def test_normal_year(self):
+        all_sessions = self.calendar.all_sessions
+
+        expected_holidays = [
+            pd.Timestamp('2018-01-01', tz='UTC'),  # New Year's Day
+            pd.Timestamp('2018-03-30', tz='UTC'),  # Good Friday
+            pd.Timestamp('2018-04-02', tz='UTC'),  # Easter Monday
+            pd.Timestamp('2018-05-07', tz='UTC'),  # May Bank Holiday
+            pd.Timestamp('2018-06-04', tz='UTC'),  # June Bank Holiday
+            pd.Timestamp('2018-12-25', tz='UTC'),  # Christmas Day
+            pd.Timestamp('2018-12-26', tz='UTC'),  # Boxing Day
+        ]
+
+        for session_label in expected_holidays:
+            self.assertNotIn(session_label, all_sessions)
+
+    def test_holidays_fall_on_weekend(self):
+        all_sessions = self.calendar.all_sessions
+
+        # Holidays falling on a weekend should be made up on the next trading
+        # day.
+        expected_holidays = [
+            # In 2017 New Year's Day fell on a Sunday, so the following Monday
+            # should be a holiday.
+            pd.Timestamp('2017-01-02', tz='UTC'),
+
+            # In 2010 Christmas fell on a Saturday, meaning Boxing Day fell on
+            # a Sunday. The following Monday and Tuesday should both be
+            # holidays.
+            pd.Timestamp('2010-12-27', tz='UTC'),
+            pd.Timestamp('2010-12-28', tz='UTC'),
+
+            # In 2016 Christmas fell on a Sunday, but again the following
+            # Monday and Tuesday should both be holidays.
+            pd.Timestamp('2016-12-26', tz='UTC'),
+            pd.Timestamp('2016-12-27', tz='UTC'),
+        ]
+
+        for session_label in expected_holidays:
+            self.assertNotIn(session_label, all_sessions)
+
+    def test_old_holidays(self):
+        """
+        Test the before and after of holidays that are no longer observed.
+        """
+        all_sessions = self.calendar.all_sessions
+
+        expected_holidays = [
+            pd.Timestamp('1996-03-18', tz='UTC'),  # St. Patrick's Day Observed
+            pd.Timestamp('2000-03-17', tz='UTC'),  # St. Patrick's Day
+            pd.Timestamp('2009-05-01', tz='UTC'),  # Labour Day
+        ]
+
+        for session_label in expected_holidays:
+            self.assertNotIn(session_label, all_sessions)
+
+        expected_sessions = [
+            pd.Timestamp('2001-03-19', tz='UTC'),  # St. Patrick's Day Observed
+            pd.Timestamp('2003-03-17', tz='UTC'),  # St. Patrick's Day
+            pd.Timestamp('2012-05-01', tz='UTC'),  # Labour Day
+        ]
+
+        for session_label in expected_sessions:
+            self.assertIn(session_label, all_sessions)
+
+    def test_ad_hoc_holidays(self):
+        # March 2, 2018 was closed due to sever weather.
+        self.assertNotIn(
+            pd.Timestamp('2018-03-02', tz='UTC'),
+            self.calendar.all_sessions,
+        )
+
+    def test_early_closes(self):
+        # The session label and close time for expected early closes.
+        expected_early_closes = [
+            # Christmas Eve on a weekday.
+            (
+                pd.Timestamp('2010-12-24', tz='UTC'),
+                pd.Timestamp('2010-12-24 12:28', tz='Europe/Dublin'),
+            ),
+            (
+                pd.Timestamp('2018-12-24', tz='UTC'),
+                pd.Timestamp('2018-12-24 12:28', tz='Europe/Dublin'),
+            ),
+            # If Christmas Eve falls on a weekend the last trading day before
+            # Christmas should be a trading day.
+            (
+                pd.Timestamp('2017-12-22', tz='UTC'),
+                pd.Timestamp('2017-12-22 12:28', tz='Europe/Dublin'),
+            ),
+            # New Year's Eve on a weekday.
+            (
+                pd.Timestamp('2010-12-31', tz='UTC'),
+                pd.Timestamp('2010-12-31 12:28', tz='Europe/Dublin'),
+            ),
+            (
+                pd.Timestamp('2018-12-31', tz='UTC'),
+                pd.Timestamp('2018-12-31 12:28', tz='Europe/Dublin'),
+            ),
+            # If New Year's Eve falls on a weekend the last trading day of the
+            # year should be a half day.
+            (
+                pd.Timestamp('2017-12-29', tz='UTC'),
+                pd.Timestamp('2017-12-29 12:28', tz='Europe/Dublin'),
+            ),
+            # March 1, 2018 was a half day due to severe weather.
+            (
+                pd.Timestamp('2018-03-01', tz='UTC'),
+                pd.Timestamp('2018-03-01 12:28', tz='Europe/Dublin'),
+            ),
+        ]
+
+        for session, expected_close in expected_early_closes:
+            self.assertEqual(
+                self.calendar.session_close(session),
+                expected_close,
+            )
+
+        expected_full_days = [
+            # Prior to 2010 Christmas Eve and New Year's Eve were full days.
+            (
+                pd.Timestamp('2009-12-24', tz='UTC'),
+                pd.Timestamp('2009-12-24 16:28', tz='Europe/Dublin'),
+            ),
+            (
+                pd.Timestamp('2009-12-31', tz='UTC'),
+                pd.Timestamp('2009-12-31 16:28', tz='Europe/Dublin'),
+            ),
+            # March 1st on any other year should be a normal day.
+            (
+                pd.Timestamp('2017-03-01', tz='UTC'),
+                pd.Timestamp('2017-03-01 16:28', tz='Europe/Dublin'),
+            ),
+        ]
+
+        for session, expected_close in expected_full_days:
+            self.assertEqual(
+                self.calendar.session_close(session),
+                expected_close,
+            )
---
+++ trading_calendars/exchange_calendar_xdub.py
@@ -0,0 +1,157 @@
+#
+# Copyright 2018 Quantopian, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from datetime import time
+from pandas import Timestamp
+from pandas.tseries.holiday import (
+    DateOffset,
+    EasterMonday,
+    GoodFriday,
+    Holiday,
+    MO,
+    previous_friday,
+    weekend_to_monday,
+)
+from pytz import timezone
+
+from .common_holidays import (
+    new_years_day,
+    european_labour_day,
+    christmas,
+    weekend_christmas,
+    boxing_day,
+    weekend_boxing_day,
+)
+from .trading_calendar import HolidayCalendar, TradingCalendar
+
+NewYearsDay = new_years_day(observance=weekend_to_monday)
+
+StPatricksDay = Holiday(
+    "St. Patrick's Day",
+    month=3,
+    day=17,
+    end_date='2001',
+    observance=weekend_to_monday,
+)
+
+LabourDay = european_labour_day(end_date='2010', observance=weekend_to_monday)
+
+MayBankHoliday = Holiday(
+    'May Bank Holiday',
+    month=5,
+    day=1,
+    offset=DateOffset(weekday=MO(1)),
+)
+JuneBankHoliday = Holiday(
+    'June Bank Holiday',
+    month=6,
+    day=1,
+    offset=DateOffset(weekday=MO(1)),
+)
+
+LastTradingDayBeforeChristmas = Holiday(
+    'Last Trading Day Before Christmas',
+    month=12,
+    day=24,
+    start_date='2010',
+    observance=previous_friday,
+)
+Christmas = christmas()
+WeekendChristmas = weekend_christmas()
+BoxingDay = boxing_day()
+WeekendBoxingDay = weekend_boxing_day()
+
+LastTradingDayOfCalendarYear = Holiday(
+    'Last Trading Day Of Calendar Year',
+    month=12,
+    day=31,
+    start_date='2010',
+    observance=previous_friday,
+)
+
+# Ad hoc closes.
+March1BadWeather = Timestamp('2018-03-01', tz='UTC')
+March2BadWeather = Timestamp('2018-03-02', tz='UTC')
+
+
+class XDUBExchangeCalendar(TradingCalendar):
+    """
+    Calendar for the Irish Stock Exchange in Dublin.
+
+    Open Time: 8:00 AM, GMT
+    Close Time: 4:28 PM, GMT
+
+    Regularly-Observed Holidays:
+      - New Years Day
+      - Good Friday
+      - Easter Monday
+      - May Bank Holiday
+      - June Bank Holiday
+      - Christmas Day
+      - St. Stephen's Day (Boxing Day)
+
+    Holidays No Longer Observed:
+      - St. Patrick's Day
+      - Labour Day
+
+    Early Closes:
+      - Christmas Eve
+      - New Year's Eve
+    """
+    name = 'XDUB'
+    tz = timezone('Europe/Dublin')
+    open_times = (
+        (None, time(8, 1)),
+    )
+    close_times = (
+        (None, time(16, 28)),
+    )
+    regular_early_close = time(12, 28)
+
+    @property
+    def regular_holidays(self):
+        return HolidayCalendar([
+            NewYearsDay,
+            StPatricksDay,
+            GoodFriday,
+            EasterMonday,
+            LabourDay,
+            MayBankHoliday,
+            JuneBankHoliday,
+            Christmas,
+            WeekendChristmas,
+            BoxingDay,
+            WeekendBoxingDay,
+        ])
+
+    @property
+    def adhoc_holidays(self):
+        return [March2BadWeather]
+
+    @property
+    def special_closes(self):
+        return [
+            (
+                self.regular_early_close,
+                HolidayCalendar([
+                    LastTradingDayBeforeChristmas,
+                    LastTradingDayOfCalendarYear,
+                ]),
+            ),
+        ]
+
+    @property
+    def special_closes_adhoc(self):
+        return [(self.regular_early_close, [March1BadWeather])]
