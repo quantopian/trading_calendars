3487fee28f1c84a8483d922e23924cb07bd1f514
Frederick Wagner <fwagner@quantopian.com>
2019-10-01 14:07:52 -0400

    TST: Add test case for 2018

--- tests/test_xjse_calendar.py
+++ tests/test_xjse_calendar.py
@@ -45,6 +45,21 @@ class XJSECalendarTestCase(ExchangeCalendarTestBase, TestCase):
             '2019-12-25',  # Christmas
             '2019-12-26',  # Day of Goodwill
         ]),
+        ('2018-01-01', '2018-12-31', [
+            '2018-01-01',  # New Year's Day
+            '2018-03-21',  # Human Rights Day
+            '2018-03-30',  # Good Friday
+            '2018-04-02',  # Family Day
+            '2018-04-27',  # Freedom Day
+            '2018-05-01',  # Workers' Day
+            '2018-06-16',  # Youth Day (falls on Saturday, not made up)
+            '2018-08-09',  # National Women's Day
+            '2018-09-24',  # Heritage Day
+            '2018-12-16',  # Day of Reconciliation
+            '2018-12-17',  # Day of Reconciliation (Monday make-up)
+            '2018-12-25',  # Christmas
+            '2018-12-26',  # Day of Goodwill
+        ]),
     ])
     def test_holidays_in_date_range(self, start, end, holiday_dates):
         holidays = {pd.Timestamp(d, tz=UTC) for d in holiday_dates}
