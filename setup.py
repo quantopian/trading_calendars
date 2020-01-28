1fe1dd3494e116422dcd7c7173adce08a6ceaa4b
Gerry Manoim <gmanoim@quantopian.com>
2020-01-28 11:19:12 -0500

    Drop windows. Update flake8 cfgs. Bump version

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ classifiers = [
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8"
     "License :: OSI Approved :: Apache Software License",
     "Intended Audience :: Science/Research",
     "Topic :: Scientific/Engineering",
