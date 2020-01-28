21d105d56c736124201fb0d7e9386a8acfb74db8
Gerry Manoim <gmanoim@quantopian.com>
2020-01-28 13:35:18 -0500

    Only run on non 24/7 calendars

--- tests/test_trading_calendar.py
+++ tests/test_trading_calendar.py
@@ -473,22 +473,23 @@ class ExchangeCalendarTestBase(object):
                 previous_session_label
             )
 
-            # Make sure we use the cache correctly
-            minutes_that_resolve_to_different_sessions = [
-                self.calendar.minute_to_session_label(minute_after_session,
-                                                      direction="next"),
-                self.calendar.minute_to_session_label(minute_after_session,
-                                                      direction="previous"),
-                self.calendar.minute_to_session_label(minute_after_session,
-                                                      direction="next"),
-            ]
+            if self.GAPS_BETWEEN_SESSIONS:
+                # Make sure we use the cache correctly
+                minutes_that_resolve_to_different_sessions = [
+                    self.calendar.minute_to_session_label(minute_after_session,
+                                                        direction="next"),
+                    self.calendar.minute_to_session_label(minute_after_session,
+                                                        direction="previous"),
+                    self.calendar.minute_to_session_label(minute_after_session,
+                                                        direction="next"),
+                ]
 
-            self.assertEqual(
-                minutes_that_resolve_to_different_sessions,
-                [next_session_label,
-                 session_label,
-                 next_session_label]
-            )
+                self.assertEqual(
+                    minutes_that_resolve_to_different_sessions,
+                    [next_session_label,
+                    session_label,
+                    next_session_label]
+                )
 
             # make sure that exceptions are raised at the right time
             with self.assertRaises(ValueError):
