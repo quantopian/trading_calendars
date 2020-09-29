e24d00331361ef2296804696b47090ab2f19e631
Gerry Manoim <gmanoim@quantopian.com>
2020-09-29 12:51:07 -0400

    fix up names, fix non return

--- trading_calendars/exchange_calendar_xcbf.py
+++ trading_calendars/exchange_calendar_xcbf.py
@@ -33,11 +33,11 @@ def good_friday_unless_christmas_nye_friday(dt):
     christmas_weekday = Christmas.observance(
         pd.Timestamp(year=dt.year, month=12, day=25)
     ).weekday()
-    nye_weekday = USNewYearsDay.observance(
+    nyd_weekday = USNewYearsDay.observance(
         pd.Timestamp(year=dt.year, month=1, day=1)
     ).weekday()
-    if christmas_weekday != 4 and nye_weekday != 4:
-        GoodFriday._apply_rule(
+    if christmas_weekday != 4 and nyd_weekday != 4:
+        return GoodFriday._apply_rule(
             pd.Timestamp(year=dt.year, month=dt.month, day=dt.day)
         )
 
