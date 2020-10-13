4c242df1bfc5970bcd6a8cc12acf252e0bf5faf5
Gerry Manoim <gmanoim@quantopian.com>
2020-10-13 18:06:37 -0400

    BENCH: Add CI benchmarks

---
+++ etc/bench.py
@@ -0,0 +1,29 @@
+import itertools
+
+import pytest
+
+from trading_calendars.calendar_utils import (
+    _default_calendar_aliases,
+    _default_calendar_factories,
+    TradingCalendarDispatcher,
+)
+
+
+def construct_all_calendars():
+    dispatcher = TradingCalendarDispatcher(
+        calendars={},
+        calendar_factories=_default_calendar_factories,
+        aliases=_default_calendar_aliases,
+    )
+
+    calendar_names = itertools.chain(
+        _default_calendar_aliases, _default_calendar_factories
+    )
+
+    for name in calendar_names:
+        assert dispatcher.get_calendar(name) is not None
+        dispatcher.deregister_calendar(name)
+
+
+def test_calendar_construction(benchmark):
+    benchmark(construct_all_calendars)
