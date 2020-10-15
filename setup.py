2054a203161c4aab05f45c8596dca59e8f622324
Richard Frank <rich@quantopian.com>
2020-10-15 09:44:20 -0400

    TST: Parallelize tests across cpus

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ if __name__ == '__main__':
                 "pytest",
                 "pytest-benchmark",
                 "parameterized",
+                "pytest-xdist",
             ],
         },
     )
